68bf1f570cfe3989f3caef8488d168c37e5a939f
FuSoraS <developer.FuSoraS@gmail.com>
2026-05-03 22:03:25 -0400

    refactor: add title, image in the DOM

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { getShowData } from "./modules/getShowdata.js"
 const ID = "2993"
 
-const datafi = await getShowData(ID)
-console.log(datafi.name)
+const show = await getShowData(ID)
+console.log(show.name)
+
+const header = document.querySelector("header")
+
+header.setHTMLUnsafe(/* html */ `
+    <img src=${show.image} alt=${show.name}>
+    <h1>${show.name}</h1>`
+)

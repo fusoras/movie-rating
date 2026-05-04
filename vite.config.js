c6c0996ae46f1094aee746cad302b5dd79b22c39
FuSoraS <developer.FuSoraS@gmail.com>
2026-05-04 12:00:56 -0400

    feat: update Vite config for proper build output and add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src",
-  base: "/",
+  base: "/movie-rating",
   publicDir: "../public",
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     assetsDir: "assets"
   },
   plugins: []
